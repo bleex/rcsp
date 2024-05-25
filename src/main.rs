3e8fd1aefeb156c0c27cc0c03e325ee849a32874
Liwiusz Ociepa <9681306+bleex@users.noreply.github.com>
2024-05-25 18:02:46 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,79 @@
+use actix_web::{error, web, App, Error, HttpResponse, HttpServer};
+use serde::Deserialize;
+use futures::StreamExt;
+
+#[actix_web::main]
+async fn main() {
+    std::env::set_var("RUST_LOG", "debug");
+    env_logger::init();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(get_index))
+            .route("/csp", web::post().to(post_csp))
+    });
+
+    println!("Serving on http://localhost:5000...");
+    server
+        .bind("127.0.0.1:5000").expect("error binding server to address")
+        .run()
+        .await
+        .expect("error running server");
+}
+
+async fn get_index() -> HttpResponse {
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(
+            r#"
+                <title>CSP Reporting server</title>
+                <form action="/csp" method="post">
+                <input type="text" name="csp"/>
+                <button type="submit">Post CSP report manually</button>
+                </form>
+            "#,
+        )
+}
+
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "kebab-case")]
+struct Csp {
+    blocked_uri: String,
+    column_number: u32,
+    disposition: String,
+    document_uri: String,
+    effective_directive: String,
+    original_policy: String,
+    referrer: String,
+    status_code: u32,
+    violated_directive: String,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "kebab-case")]
+struct CspReport {
+    csp_report: Csp,
+}
+
+const MAX_SIZE: usize = 262_144; // max payload size is 256k
+
+//async fn post_csp(info: web::Json<CspReport>) -> HttpResponse {
+async fn post_csp(mut info: web::Payload) -> Result<HttpResponse, Error> {
+    let response =
+        format!("Nothing");
+    let mut body = web::BytesMut::new();
+    while let Some(chunk) = info.next().await {
+        let chunk = chunk?;
+        // limit max size of in-memory payload
+        if (body.len() + chunk.len()) > MAX_SIZE {
+            return Err(error::ErrorBadRequest("overflow"));
+        }
+        body.extend_from_slice(&chunk);
+    }
+    let obj = serde_json::from_slice::<CspReport>(&body)?;
+    println!("{:?}", obj);
+    Ok(HttpResponse::Ok()
+        .content_type("text/html")
+        .body(response))
+}
+
